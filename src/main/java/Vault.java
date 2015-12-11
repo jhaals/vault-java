157478fe14dc35d7f92213a95076faac5ca58423
Johan Haals <johan.haals@gmail.com>
2015-12-11 11:10:20 +0100

    Add javadoc

--- src/main/java/Vault.java
+++ src/main/java/Vault.java
@@ -10,22 +10,45 @@ import javax.ws.rs.core.Response;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Java API for Hashicorp's Vault project. A tool for managing secrets.
+ * @see <a href="https://vaultproject.io/">https://vaultproject.io/</a>
+ */
 public class Vault {
     private final String vaultToken;
     private final Client client;
     private WebTarget baseTarget;
 
+    /**
+     * Initialize Vault
+     * @param vaultServer http address to vault server, example: http://127.0.0.1:8200
+     * @param vaultToken vault token used to authenticate requests
+     */
     public Vault(String vaultServer, String vaultToken) {
         this.vaultToken = vaultToken;
         this.client = ClientBuilder.newBuilder().register(JacksonJaxbJsonProvider.class).build();
         this.baseTarget = client.target(vaultServer);
     }
+
+    /**
+     * Initialize Vault with custom Jersey HTTP client, for fine tuning and proxy configurations
+     * @param vaultServer http address to vault server, example: http://127.0.0.1:8200
+     * @param vaultToken  vault token used to authenticate requests
+     * @param client custom Jersey client
+     */
     public Vault(String vaultServer, String vaultToken, Client client) {
         this.vaultToken = vaultToken;
         this.client = client;
         this.baseTarget = client.target(vaultServer);
     }
 
+    /**
+     * Read value from vault
+     *
+     * @param path the vault path where secret is stored
+     * @return the response object containing your secret
+     * @throws VaultException if API returns anything except 200
+     */
     public VaultResponse read(String path) {
         WebTarget target = baseTarget.path(String.format("/v1/%s", path));
         Response response = null;
@@ -46,6 +69,15 @@ public class Vault {
         }
     }
 
+    /**
+     * Write key/value secret to vault
+     *
+     * @param path   where to write secret
+     * @param secret A key, paired with an associated value, to be held at the given location.
+     *               Multiple key/value pairs can be specified, and all will be returned on a read operation.
+     *               The generic backend use 'value' as key
+     * @throws VaultException if operation fails
+     * */
     public void write(String path, HashMap<String, String> secret) {
         WebTarget target = baseTarget.path(String.format("/v1/%s", path));
         Response response = null;
@@ -66,6 +98,11 @@ public class Vault {
         }
     }
 
+    /**
+     * Delete path from vault
+     * @param path to be deleted
+     * @throws VaultException containing error reason
+     */
     public void delete(String path) {
         WebTarget target = baseTarget.path(String.format("/v1/%s", path));
         Response response = null;
