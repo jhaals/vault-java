4492db3c3d767a5b0f31936769526a96edc3d37b
Johan Haals <johan.haals@gmail.com>
2015-12-11 14:09:32 +0100

    More tests

--- src/test/java/VaultTest.java
+++ src/test/java/VaultTest.java
@@ -1,9 +1,44 @@
 import junit.framework.TestCase;
 
+import java.util.HashMap;
+
 public class VaultTest extends TestCase {
 
-    public void testGetToken() throws Exception {
-        Vault vault = new Vault("http://127.0.0.1:8200", "c7e6d2f3-dc1a-a841-cf29-0cf7bec8ed42");
-        System.out.println(vault.read("aws/creds/deploy").toString());
+    private String token = System.getenv("VAULT_TOKEN");
+
+    public void testWrite() throws Exception {
+        Vault vault = new Vault("http://127.0.0.1:8200", token);
+        HashMap<String, String> data = new HashMap<>();
+        data.put("value", "hello");
+        vault.write("secret/hello", data);
     }
+
+    public void testRead() throws Exception {
+        Vault vault = new Vault("http://127.0.0.1:8200", token);
+        VaultResponse result = vault.read("secret/hello");
+        assertEquals(result.getData().get("value"), "hello");
+    }
+
+    public void testDelete() throws Exception {
+        Vault vault = new Vault("http://127.0.0.1:8200", token);
+        vault.delete("secret/hello");
+        try {
+            vault.read("secret/hello");
+            fail("Expected VaultException");
+        } catch (VaultException e) {
+            assertEquals(e.getStatusCode(), 404);
+
+        }
+    }
+
+    public void testReadWithInvalidToken() throws Exception {
+        Vault vault = new Vault("http://127.0.0.1:8200", "invalid");
+        try {
+            vault.read("secret/hello");
+            fail("Expected VaultException");
+        } catch (VaultException e) {
+            assertEquals(e.getStatusCode(), 403);
+        }
+    }
+
 }
